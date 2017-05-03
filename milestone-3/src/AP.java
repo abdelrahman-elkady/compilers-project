1a9f1c82a23900655aad67e63498829311c554d0
kady <elkadyabdelrahman@gmail.com>
2017-05-03 14:33:24 +0200

    Init milestone 3

---
+++ milestone-3/src/AP.java
@@ -0,0 +1,44 @@
+
+import java_cup.parser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class AP {
+
+	public static void main(String[] args) {
+
+		String inFile = "data/Go1.in";
+		String outFile = "data/Go1.out";
+
+		if (args.length > 1) {
+			inFile = args[0];
+		}
+
+		try {
+			FileReader fis = new FileReader(inFile);
+			BufferedReader bis = new BufferedReader(fis);
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+
+			@SuppressWarnings("deprecation")
+			parser parser = new parser(new Lexer(bis));
+
+			try {
+				parser.parse();
+				writer.write("Done");
+			} catch (Exception e) {
+				writer.write("ParseError");
+			}
+
+			fis.close();
+			bis.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
