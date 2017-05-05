96ff19f440144dec0657fae43733834f107073e7
Mohamed Mehany <mohamed-mehany@hotmail.com>
2017-05-05 18:18:09 +0200

    Added stuff

--- milestone-3/src/run.sh
+++ milestone-3/src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+rm -rf *.class
 java -jar java-cup-11b.jar parser.cup
 javac JLex/Main.java  && java JLex.Main Lexer
 cp data/Go"$1".in data/safe.in
@@ -7,5 +7,5 @@ cp data/Go"$1".in data/safe.in
 echo '\n\n'
 echo '========================================= Now Run AP.java on safe.in ========================================='
 echo '\n\n'
-javac -cp .:* sym.java Lexer.java parser.java AP.java  && java -cp .:* AP
-rm -rf **/*.class
+javac -cp .:* *.java  && java -cp .:* AP
+rm -rf *.class
