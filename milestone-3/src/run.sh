a5da37a66bc159275995c76c3dfb521f532cbbd0
Mohamed Mehany <mohamed-mehany@hotmail.com>
2017-05-05 16:50:02 +0200

    Added to parser.cup and modified shell script

--- milestone-3/src/run.sh
+++ milestone-3/src/run.sh
@@ -2,10 +2,10 @@
 
 java -jar java-cup-11b.jar parser.cup
 javac JLex/Main.java  && java JLex.Main Lexer
-cp ../data/Go"$1".in ../data/safe.in
+cp data/Go"$1".in data/safe.in
 
 echo '\n\n'
 echo '========================================= Now Run AP.java on safe.in ========================================='
 echo '\n\n'
-
+javac -cp .:* sym.java Lexer.java parser.java AP.java  && java -cp .:* AP
 rm -rf **/*.class
